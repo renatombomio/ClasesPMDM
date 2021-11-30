37021b7742a118a67a803fe62a7ea471a04619ae
renatombomio <jesus.mbomio@cesjuanpablosegundo.es>
2021-11-30 12:27:45 +0100

    update

---
+++ REPASO/app/src/main/java/com/example/repaso/DetailActivity.java
@@ -0,0 +1,24 @@
+package com.example.repaso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.repaso.utils.Marca;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private ImageView imagen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        imagen = findViewById(R.id.imagen_detalle);
+        Marca marcaRecuperada = (Marca) getIntent().getExtras().getSerializable("marca");
+        //Que imagen pongo aquí
+
+        imagen.setImageResource(marcaRecuperada.getImagen());
+    }
+}

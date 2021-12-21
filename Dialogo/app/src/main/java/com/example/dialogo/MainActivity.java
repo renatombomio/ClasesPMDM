4b15a138e79b47b69813b3a2e33776b1bf4e91f2
renatombomio <jesus.mbomio@cesjuanpablosegundo.es>
2021-12-21 10:27:32 +0100

    update

--- Dialogo/app/src/main/java/com/example/dialogo/MainActivity.java
+++ Dialogo/app/src/main/java/com/example/dialogo/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.dialogo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.dialogo.dialog.DialogoConfirmacion;
+import com.example.dialogo.dialog.DialogoInformacion;
+import com.example.dialogo.dialog.DialogoListas;
+import com.example.dialogo.dialog.DialogoPersonalizado;
+import com.example.dialogo.utils.Persona;
+
+public class MainActivity extends AppCompatActivity implements DialogoPersonalizado.OnDialogoPersoListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_op1:
+                //Toast.makeText(getApplicationContext(),"Opcion1", Toast.LENGTH_SHORT).show();
+                // dialogo info
+                DialogoInformacion dialogoInformacion = new DialogoInformacion();
+                dialogoInformacion.show(getSupportFragmentManager(), "informacion");
+
+                break;
+            case R.id.menu_op2:
+                //Toast.makeText(getApplicationContext(),"Opcion2", Toast.LENGTH_SHORT).show();
+                DialogoConfirmacion dialogoConfirmacion = new DialogoConfirmacion();
+                dialogoConfirmacion.show(getSupportFragmentManager(), "confirmacion");
+                break;
+            case R.id.menu_op3:
+                //Toast.makeText(getApplicationContext(),"Opcion3", Toast.LENGTH_SHORT).show();
+                DialogoListas dialogoListas = new DialogoListas();
+                dialogoListas.show(getSupportFragmentManager(),"listas");
+
+                break;
+            case R.id.menu_op4:
+                Toast.makeText(getApplicationContext(),"Opcion4", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onPersonaSelected(Persona persona) {
+        Toast.makeText(getApplicationContext(),"comunicacion ok",Toast.LENGTH_SHORT).show();
     }
 }

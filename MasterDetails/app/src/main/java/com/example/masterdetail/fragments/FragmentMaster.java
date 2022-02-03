919c68f98744254d1e80393bce08b3bb1a7626bb
renatombomio <jesus.mbomio@cesjuanpablosegundo.es>
2022-02-04 00:31:28 +0100

    update

--- MasterDetails/app/src/main/java/com/example/masterdetail/fragments/FragmentMaster.java
+++ MasterDetails/app/src/main/java/com/example/masterdetail/fragments/FragmentMaster.java
@@ -1,9 +1,11 @@
 package com.example.masterdetail.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -15,18 +17,25 @@ import com.example.masterdetail.R;
 
 import java.util.ArrayList;
 
-public class FragmentMaster extends Fragment{
+public class FragmentMaster extends Fragment {
+
 
     private View view;
     private ListView listView;
     private ArrayAdapter adapter;
     private ArrayList datos;
+    private OnElementoListener listener;
 
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        listener = (OnElementoListener) context;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.from(getContext()).inflate(R.layout.fragment_master, container, false);
+        view = inflater.from(getContext()).inflate(R.layout.fragment_master,container,false);
         return view;
     }
 
@@ -34,10 +43,24 @@ public class FragmentMaster extends Fragment{
     public void onStart() {
         super.onStart();
         listView = view.findViewById(R.id.lista_master);
-        datos = new ArrayList<>();
+        datos = new ArrayList();
         adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, datos);
-        for (int i = 0; i < 100; i++) {
-
+        for (int i = 1; i < 100; i++) {
+            datos.add("Opcion "+i);
+            adapter.notifyDataSetChanged();
         }
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Toast.makeText(getContext(), listView.getAdapter().getItem(i).toString(), Toast.LENGTH_SHORT).show();
+                listener.onElementoSelected((String) listView.getItemAtPosition(i));
+            }
+        });
+    }
+
+    public interface OnElementoListener{
+        void onElementoSelected(String elemento);
     }
 }

19bdcc670b9c9ee8ff125c3f48144ff94029ea59
renatombomio <jesus.mbomio@cesjuanpablosegundo.es>
2022-05-16 17:41:31 +0200

    update

--- Anima2/app/src/main/java/com/example/animados/LoginActivity.java
+++ Anima2/app/src/main/java/com/example/animados/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.animados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        //(Para cambiar el color del icono)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+
+
         setContentView(R.layout.activity_login);
     }
+
+    public void onLoginClick (View view){
+        startActivity(new Intent(this, RegistroActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+    }
+
 }
--- Anima2/app/src/main/java/com/example/animados/RegistroActivity.java
+++ Anima2/app/src/main/java/com/example/animados/RegistroActivity.java
@@ -2,7 +2,12 @@ package com.example.animados;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class RegistroActivity extends AppCompatActivity {
 
@@ -10,5 +15,23 @@ public class RegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        changeStatusBarColor();
+    }
+
+    public void changeStatusBarColor() {
+        //(Para cambiar el color del icono)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.register_bk_color));
+
+        }
+    }
+
+    public void onLoginClick(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+        overridePendingTransition(R.anim.slide_in_left, android.R.anim.slide_out_right);
     }
 }
